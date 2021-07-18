af2aa0d6cee705d24066bbc66d25da6e2fe5da4a
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-07-18 22:29:27 +0300

    init

--- RunAwesome/ViewControllers/BaseViewController.swift
+++ RunAwesome/ViewControllers/BaseViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BaseViewController.swift
 //  RunAwesome
 //
 //  Created by Ramill Ibragimov on 18.07.2021.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange.withAlphaComponent(0.2)
     }
-
-
+  
 }
-
